509ba7f77b527ee3438772aec088f247c7641364
sash713 <sashreek123@hotmail.com>
2020-04-04 08:54:37 +0530

    Added script to send message to cloud

---
+++ cloud-integration/sendmsg.py
@@ -0,0 +1,39 @@
+from google.cloud import pubsub_v1
+import datetime
+import json
+import time
+
+project_id = "<your prject id>" # enter your project id here
+topic_name = "<topic name>" # enter the name of the topic that you created
+
+publisher = pubsub_v1.PublisherClient()
+topic_path = publisher.topic_path(project_id, topic_name)
+
+futures = dict()
+
+def get_callback(f, data):
+    def callback(f):
+        try:
+            # print(f.result())
+            futures.pop(data)
+        except:
+            print("Please handle {} for {}.".format(f.exception(), data))
+
+    return callback
+
+while True:
+    time.sleep(3)
+    timenow = float(time.time())
+    # timenow = datetime.datetime.now()
+    data = {"time":timenow} # This is the JSON message that will be sent to the cloud
+    print(data)
+    # When you publish a message, the client returns a future.
+    future = publisher.publish(
+        topic_path, data=(json.dumps(data)).encode("utf-8")) # data must be a bytestring.
+    # Publish failures shall be handled in the callback function.
+    future.add_done_callback(get_callback(future, data))
+# Wait for all the publish futures to resolve before exiting.
+while futures:
+    time.sleep(5)
+
+print("Published message with error handler.")
